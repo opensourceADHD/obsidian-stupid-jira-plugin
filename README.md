dfbf7ff19ccd1f8a8f7df10a5fb64b90672bb09c
Pyro - OpenSourceADHD <pyro@os80.dev>
2024-02-15 23:28:54 -0500

    base tag recognition working

--- README.md
+++ README.md
@@ -1,96 +1,49 @@
-# Obsidian Sample Plugin
+# Stupid Jira Plugin
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+This is a quick and dirty plugin that lets you mention Jira tickets in your notes, and then get links automatically created that go to your helpdesk. It doesn't use the API or have any kind of smart integration, just good old regex and replace.
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+I wanted something I could very quickly turn on for my personal notes vault at work, and there are some existing plugins that would integrate well with Jira from what I can see, but I don't actually need that at the moment, and getting the plugin vetted and approved my the security team isn't worth the effort. So enter the **Stupid Jira Plugin**.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+# Settings
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+![Settings](./sjp_settings_20240201.png)
 
-## First time developing plugins?
+## Base URL
 
-Quick starting guide for new plugin devs:
+Set your BaseURL for what you'd want to prefix as your link. Basically go to a ticket on your helpdesk, and copy everything except the ticket number.
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+## Prefixes
 
-## Releasing new releases
+Add as many Prefixes as you want to include in the matching. No need to include the hyphen at the end.
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+## Use Styles
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+If set, this applies some styling to the resulting links, you can disable this if you want plain links. Alternatively you can add your own styles and override the .os80-sjp class in your own CSS.
 
-## Adding your plugin to the community plugin list
+# Ideas
 
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+-   Support multiple cursors
+-   Ability to customize per-file with Frontmatter settings
+    -   Base URL
+    -   Prefixes
+    -   Disable SJP
+-   Case Insensitivity switch in settings
+-   Render in other common plugins
+    -   Kanban
+-   Associate prefixes to alternate URLs
+-   Optional icon for links (Jira logo) or provided by user
+-   Handle certain stylings
+    -   italics, bold, strikethru,
+    -   highlight
+    -   Block Quotes
 
-## How to use
+## Known Issues
 
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
+-   Tags not (always) rendering in Callouts in Live Preview Mode
+-   Sometimes tags can act oddly if there are multiple tags, but the earlier tags are not surrounded by whitespace.
+-   Tags in Table Headers show up twice in live edit, and are not rendered in live preview.
+-   Toggling "Use Styles" in settings requires a refresh to take effect for Reader Mode.
 
-## Manually installing the plugin
+# References
 
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+Shoutout to Andrew Zito for their [Obsidian Nice Keyboard Shortcuts](https://github.com/andyzito/obsidian-nice-kbds) plugin and [questions on the Obsidian forum](https://forum.obsidian.md/t/plugin-api-expose-live-edit-functionality-for-extension/73447/7) for asking the questions I forgot to ask months ago for this. Helped me figure out how to make this work in Editor mode and Reader mode! And also to Ryota Ushio for helping Andrew!
--- main.ts
+++ main.ts
@@ -1,134 +1,498 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Modal,
+	Notice,
+	Plugin,
+	ButtonComponent,
+	PluginSettingTab,
+	editorLivePreviewField,
+	Setting,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
+import {
+	EditorView,
+	Decoration,
+	DecorationSet,
+	WidgetType,
+} from "@codemirror/view";
 
-interface MyPluginSettings {
-	mySetting: string;
+import { syntaxTree } from "@codemirror/language";
+
+import {
+	Range,
+	StateField,
+	RangeSetBuilder,
+	Transaction,
+	Extension,
+} from "@codemirror/state";
+
+import { debounce } from "obsidian";
+
+interface StupidJiraSettings {
+	useStyles: boolean;
+	prefixList: string[];
+	baseURL: string;
+	// clientURL: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: StupidJiraSettings = {
+	useStyles: true,
+	prefixList: ["HD"],
+	baseURL: "https://jira.example.com",
+	// clientURL: "https://jira.example.com/helpdesk",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-	async onload() {
-		await this.loadSettings();
+class StupidJiraSettingTab extends PluginSettingTab {
+	plugin: StupidJira;
+	prefixContainer: HTMLElement;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: StupidJira) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
+		let inTimeOut = false;
 
 		containerEl.empty();
 
+		containerEl.createEl("h1", { text: "Stupid Jira Settings" });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Styles")
+			.setDesc("Use Stupid Jira styles to format the linked tags.")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.useStyles)
+					.onChange(async (value) => {
+						this.plugin.settings.useStyles = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Jira Base URL")
+			.setDesc("The base URL for your jira agent view.")
+			.addTextArea(
+				(text) =>
+					text
+						.setPlaceholder("https://jira.example.com")
+						.setValue(this.plugin.settings.baseURL)
+						.onChange(
+							debounce(async (value) => {
+								// this.plugin.settings.baseURL = value;
+								this.plugin.settings.baseURL = value.replace(
+									/\/$/,
+									""
+								);
+								await this.plugin.saveSettings();
+								text.setValue(this.plugin.settings.baseURL);
+							}, 500)
+						)
+				// .onChange(async (value) => {
+				// 	// this.plugin.settings.baseURL = value;
+				// 	this.plugin.settings.baseURL =
+				// 		this.plugin.settings.baseURL.replace(/\/$/, "");
+				// 	await this.plugin.saveSettings();
+				// })
+			);
+
+		// new Setting(containerEl)
+		// 	.setName("Jira Customer URL")
+		// 	.setDesc("The base URL for customer facing helpdesk.")
+		// 	.addText((text) =>
+		// 		text
+		// 			.setPlaceholder("https://jira.example.com/helpdesk/")
+		// 			.setValue(this.plugin.settings.clientURL)
+		// 			.onChange(async (value) => {
+		// 				this.plugin.settings.clientURL = value;
+		// 				await this.plugin.saveSettings();
+		// 			})
+		// 	);
+
+		new Setting(containerEl)
+			.setName("Target Prefixes")
+			.setDesc("Add the prefixes you want to target.")
+			.addButton((btn) =>
+				btn
+					.setButtonText("Add Prefix")
+					.setCta()
+					.onClick(async () => {
+						new AddJiraPrefixModal(this.app, (result) => {
+							this.plugin.settings.prefixList.push(result);
+							this.display();
+							this.plugin.saveSettings();
+						}).open();
+					})
+			);
+
+		const prefixContainer = containerEl.createDiv({
+			cls: "os80-prefix-container ",
+		});
+
+		prefixContainer.id = "prefixContainer";
+		for (const prefix of this.plugin.settings.prefixList) {
+			const pill = prefixContainer.createDiv({
+				cls: "os80-pill",
+			});
+
+			pill.createSpan({
+				text: prefix,
+				cls: "os80-pill-text",
+			});
+
+			new ButtonComponent(pill)
+				.setIcon("trash-2")
+				.setClass("clickable-icon")
+				.setClass("os80-clickable-icon")
+				.onClick(() => {
+					const index = d;
+					this.plugin.settings.prefixList.indexOf(prefix);
+					if (index > -1) {
+						this.plugin.settings.prefixList.splice(index, 1);
+						this.plugin.saveSettings();
+						this.display();
+					}
+				});
+		}
+	}
+}
+
+export class AddJiraPrefixModal extends Modal {
+	result: string;
+	onSubmit: (result: string) => void;
+
+	constructor(app: App, onSubmit: (result: string) => void) {
+		super(app);
+		this.onSubmit = onSubmit;
+	}
+
+	onOpen() {
+		const { contentEl } = this;
+
+		contentEl.createEl("h1", { text: "What's the ticket prefix?" });
+		contentEl.createEl("p", {
+			text: 'Example: If your tickets are formatted HD-1080, use "HD"',
+		});
+
+		new Setting(contentEl).setName("Prefix").addText((text) =>
+			text.onChange((value) => {
+				this.result = value;
+			})
+		);
+
+		new Setting(contentEl).addButton((btn) =>
+			btn
+				.setButtonText("Submit")
+				.setCta()
+				.onClick(() => {
+					this.close();
+					this.onSubmit(this.result);
+				})
+		);
+	}
+
+	onClose() {
+		let { contentEl } = this;
+		contentEl.empty();
+	}
+}
+
+class JiraTagWidget extends WidgetType {
+	constructor(private key: string, private settings: StupidJiraSettings) {
+		super();
+	}
+
+	toDOM() {
+		return new JiraTagFactory(this.settings).getElement(this.key);
+	}
+
+	toHTML() {
+		return new JiraTagFactory(this.settings).getHTML(this.key);
+	}
+}
+
+class JiraTagFactory {
+	constructor(private settings: StupidJiraSettings) {}
+
+	getClassName() {
+		return this.settings.useStyles ? "os80-sjp" : "";
+	}
+
+	getMark() {
+		return Decoration.mark({
+			inclusive: true,
+			class: this.getClassName(),
+			tagName: "a",
+		});
+	}
+
+	getWidget(key: string) {
+		return new JiraTagWidget(key, this.settings);
+	}
+
+	getElement(key: string) {
+		const element = document.createElement("a");
+		element.className = this.getClassName();
+		element.innerText = key;
+
+		// Can uncomment this to add the little link icon after the generated links
+		// element.innerHTML += '<span class="cm-formatting cm-formatting-link-string cm-string cm-url external-link" contenteditable="false"></span>';
+		element.href = `${this.settings.baseURL}/browse/${key}`;
+		return element;
+	}
+
+	getHTML(key: string) {
+		return this.getElement(key).outerHTML;
+	}
+}
+
+const getStupidJiraStateField = (settings: StupidJiraSettings) =>
+	StateField.define<DecorationSet>({
+		create() {
+			return Decoration.none;
+		},
+
+		update(prev: DecorationSet, transaction: Transaction): DecorationSet {
+			const isSourceMode = !transaction.state.field(
+				editorLivePreviewField
+			);
+			if (isSourceMode) return Decoration.none;
+
+			const indices: Record<number, Range<Decoration>[]> = {}; // This will hold decorations mapped by combo.
+
+			const ignoreRanges: [number, number][] = [];
+
+			const builder = new RangeSetBuilder<Decoration>();
+
+			let prefixRegex = new RegExp(
+				`(\\b${settings.prefixList.join("|")})-\\d+\\b`,
+				"g"
+			);
+
+			const activeNode = transaction.state.selection.main;
+
+			for (const range of transaction.state.selection.ranges) {
+				ignoreRanges.push([range.from, range.to]);
+			}
+
+			syntaxTree(transaction.state).iterate({
+				enter(node) {
+					if (
+						node.name.match(
+							/^hmd-table-sep|^header|^quote|^list|formatting/
+						)
+					)
+						return;
+
+					const nodeText = transaction.state.doc.sliceString(
+						node.from,
+						node.to
+					);
+
+					if (
+						node.name.match(
+							/comment|hashtag|code|escape|strikethrough|callout|quote/
+						)
+					) {
+						ignoreRanges.push([node.from, node.to]);
+					}
+
+					const matches = nodeText.match(prefixRegex);
+
+					let lastIndex = 0;
+					if (matches) {
+						for (const match of matches) {
+							const from =
+								node.from + nodeText.indexOf(match, lastIndex);
+							const to = from + match.length;
+
+							lastIndex = to;
+
+							if (
+								from <= activeNode.from &&
+								activeNode.to <= to
+							) {
+								continue;
+							}
+
+							indices[from] = indices[from] || [];
+
+							indices[from].push(
+								Decoration.replace({
+									widget: new JiraTagWidget(match, settings),
+								}).range(from, to)
+							);
+						}
+					}
+				},
+			});
+
+			const uniqueIgnoreRanges = Array.from(
+				new Set(ignoreRanges.map((range) => range.join("-")))
+			).map((range) => range.split("-").map(Number));
+
+			for (const from in indices) {
+				for (const decoration of indices[from]) {
+					if (
+						!uniqueIgnoreRanges.some(
+							(range) =>
+								range[0] <= decoration.from &&
+								range[1] >= decoration.to
+						)
+					) {
+						builder.add(
+							decoration.from,
+							decoration.to,
+							decoration.value
+						);
+					}
+				}
+			}
+
+			return builder.finish();
+		},
+
+		provide(field: StateField<DecorationSet>): Extension {
+			return EditorView.decorations.from(field);
+		},
+	});
+
+const stupidJiraPostProcessor =
+	(settings: StupidJiraSettings) => (element: HTMLElement, context: any) => {
+		// if (context.displayMode == false) {
+		// In edit mode.
+		// }
+
+		const replaceInnerHTMLforJiraTag = (el: HTMLElement) => {
+			const processStupidJiratag = (node: HTMLElement) => {
+				const ignoreElements = ["CODE", "PRE", "DEL"];
+				if (
+					ignoreElements.includes(node.nodeName) ||
+					node.classList.contains("tag") ||
+					node.classList.contains("cm-comment")
+				)
+					return node.outerHTML;
+
+				const prefixRegex = new RegExp(
+					`(\\b${settings.prefixList.join("|")})-\\d+\\b`,
+					"g"
+				);
+
+				let newInnerHTML = "";
+
+				for (let childNode of Array.from(node.childNodes)) {
+					// console.table({
+					// 	"Node Type": childNode.nodeType,
+					// 	Name: childNode.nodeName,
+					// 	Value: childNode.nodeValue,
+					// });
+					if (childNode.nodeType === Node.TEXT_NODE) {
+						// console.log("Processing text node");
+						const matches = childNode.nodeValue?.match(prefixRegex);
+						if (matches) {
+							// console.log(matches.length);
+							let lastMatchIndex = 0;
+							for (const match of matches) {
+								const matchIndex = childNode.nodeValue?.indexOf(
+									match,
+									lastMatchIndex
+								);
+								const jiraTag = new JiraTagWidget(
+									match,
+									settings
+								);
+								if (matchIndex) {
+									const slice = childNode.nodeValue?.slice(
+										lastMatchIndex,
+										matchIndex
+									);
+									// console.log("Match slice: ");
+									newInnerHTML += slice;
+
+									// newInnerHTML += `<a href="${settings.baseURL}/browse/${match}" class="os80-sjp">${match}</a><span class="cm-formatting cm-formatting-link-string cm-string cm-url external-link" contenteditable="false"></span>`;
+									lastMatchIndex = matchIndex + match.length;
+								} else {
+									lastMatchIndex = match.length;
+								}
+								newInnerHTML += jiraTag.toHTML();
+							}
+							newInnerHTML +=
+								childNode.nodeValue?.slice(lastMatchIndex);
+						} else {
+							newInnerHTML += childNode.nodeValue;
+						}
+					} else if (childNode.nodeType === Node.ELEMENT_NODE) {
+						newInnerHTML += processStupidJiratag(
+							childNode as HTMLElement
+						);
+					}
+				}
+				// console.groupEnd();
+
+				node.innerHTML = newInnerHTML;
+
+				// Do stuff to find and replace what we need.
+				return node.outerHTML;
+			};
+
+			el.innerHTML = processStupidJiratag(el);
+		};
+
+		const selector =
+			"p,div.callout-title-inner,td,div.table-cell-wrapper,li,h1,h2,h3,h4,h5,h6";
+		if (element.matches(selector)) {
+			replaceInnerHTMLforJiraTag(element);
+		} else {
+			for (const el of element.findAll(selector)) {
+				if (el.innerText) {
+					replaceInnerHTMLforJiraTag(el);
+				}
+			}
+		}
+	};
+
+export default class StupidJira extends Plugin {
+	settings: StupidJiraSettings;
+
+	async onload() {
+		await this.loadSettings();
+		this.addSettingTab(new StupidJiraSettingTab(this.app, this));
+
+		if (this.settings.prefixList.length <= 0) {
+			new Notice(
+				`Stupid Jira Plugin:ds You don't have any jira prefixes added. Add one in settings`
+			);
+			return;
+		}
+		this.settings.regex = new RegExp(
+			`(\\b${this.settings.prefixList.join("|")})-\\d+\\b`,
+			"g"
+		);
+
+		// The editor extension handles live editing.
+		this.registerEditorExtension(getStupidJiraStateField(this.settings));
+
+		// The post-processor handles reading view and live edit callouts.
+		this.registerMarkdownPostProcessor(
+			stupidJiraPostProcessor(this.settings)
+		);
+	}
+
+	onunload() {}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 }
